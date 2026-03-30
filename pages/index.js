b8fc7fd2e133db26540d0847256e989623dbf62b
Lucas Pereira <lucascoelho441@gmail.com>
2026-03-29 23:30:29 -0300

    pages folder and index creation

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Depois disto, ouvi a voz do Senhor, que dizia: A quem enviarei, e quem há de ir por nós? Então disse eu: Eis-me aqui, envia-me.</h1>
+}
+
+export default Home
